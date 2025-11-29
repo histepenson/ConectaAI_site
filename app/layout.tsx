04f950881c1d381516d66da013e876dbe37ce57f
histepenson <histepenson1010@gmail.com>
2025-11-28 21:13:45 -0300

    ajustes

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,32 +26,26 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) - Google Ads */}
         <Script
-          id="gtm-script"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17766485655"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXXX');`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17766485655');
+            `,
           }}
         />
-        {/* End Google Tag Manager */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         {children}
       </body>
     </html>
